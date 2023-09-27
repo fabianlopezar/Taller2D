72ff2b5db841c525cc34910e85595944df2af98d
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2023-09-26 21:41:09 -0500

    [ADD] Movement and Anim.

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashTime;
+
+    private float xInput;
+    private int facingDir=1;
+    private bool facingRight= true;
+  
+    [Header("Collision info")]
+    private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    void Update()
+    {
+        Movement();
+        CheckInput();
+
+        dashTime -= Time.deltaTime;
+  
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashTime = dashDuration;
+        }
+       
+        CollisionChecks();
+
+        FlipController();
+        AnimatorControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(xInput*dashSpeed,0);
+        }
+        else
+        {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if(isGrounded)
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    public void AnimatorControllers()
+    {
+      bool  isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+    }
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+    private void FlipController()
+    {
+if(rb.velocity.x>0 && !facingRight)
+        {
+            Flip();
+        }else if(rb.velocity.x<0 && facingRight)
+        {
+            Flip();
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x,transform.position.y-groundCheckDistance));
+    }
+
+}
